4ef8d11b4726c3e0f7881931ea1eff9509fc9824
Adınız Soyadınız <email@adresiniz.com>
2025-02-24 14:31:50 +0300

    Vercel yapılandırması api dizini için güncellendi

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add routing middleware
+$app->addRoutingMiddleware();
+
+// CORS middleware - moved before routes
+$app->add(function (Request $request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        ->withHeader('Access-Control-Allow-Credentials', 'true');
+});
+
+// OPTIONS request handler
+$app->options('/{routes:.+}', function (Request $request, Response $response) {
+    return $response;
+});
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Routes
+require __DIR__ . '/../src/routes/api.php';
+
+$app->run(); 
